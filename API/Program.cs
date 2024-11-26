2886c05768f572819deafac51c22ed7107c86932
Neeyo <jakub.nycz99@gmail.com>
2024-11-26 11:43:51 +0100

    Excepiton handling + CORS

--- API/Program.cs
+++ API/Program.cs
@@ -2,94 +2,95 @@ using System.Text;
 using API.Data;
 using API.Entities;
 using API.Interfaces;
+using API.Middleware;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
-internal class Program
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddCors();
+
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IMountainRepository, MountainRepository>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddDbContext<DataContext>(opt =>
 {
-    private static async Task Main(string[] args)
+    opt.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 40)));
+});
+
+builder.Services.AddIdentityCore<AppUser>(opt =>
     {
-        var builder = WebApplication.CreateBuilder(args);
+        opt.Password.RequireNonAlphanumeric = false;
+    })
+        .AddRoles<AppRole>()
+        .AddRoleManager<RoleManager<AppRole>>()
+        .AddEntityFrameworkStores<DataContext>();
 
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-
-        builder.Services.AddScoped<ITokenService, TokenService>();
-        builder.Services.AddScoped<IUserRepository, UserRepository>();
-        builder.Services.AddScoped<IMountainRepository, MountainRepository>();
-        builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-        builder.Services.AddDbContext<DataContext>(opt =>
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
         {
-            opt.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
-                new MySqlServerVersion(new Version(8, 0, 40)));
-        });
-
-        builder.Services.AddIdentityCore<AppUser>(opt =>
+            var tokenKey = builder.Configuration["TokenKey"] ??
+                throw new Exception("Token key not found");
+            options.TokenValidationParameters = new TokenValidationParameters
             {
-                opt.Password.RequireNonAlphanumeric = false;
-            })
-                .AddRoles<AppRole>()
-                .AddRoleManager<RoleManager<AppRole>>()
-                .AddEntityFrameworkStores<DataContext>();
-
-        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options =>
-                {
-                    var tokenKey = builder.Configuration["TokenKey"] ??
-                        throw new Exception("Token key not found");
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
-                        ValidateIssuer = false,
-                        ValidateAudience = false
-                    };
-                }
-            );
-
-        builder.Services.AddAuthorizationBuilder()
-            .AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
-
-        var app = builder.Build();
-
-        app.UseAuthentication();
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        using var scope = app.Services.CreateScope();
-        var services = scope.ServiceProvider;
-        try
-        {
-            var context = services.GetRequiredService<DataContext>();
-            var userManager = services.GetRequiredService<UserManager<AppUser>>();
-            var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
-
-            await context.Database.MigrateAsync();
-
-            await roleManager.CreateAsync(new AppRole { Name = "User" });
-            await roleManager.CreateAsync(new AppRole { Name = "Admin" });
-
-            var adminUser = new AppUser
-            {
-                UserName = "admin",
-                KnownAs = "Admin"
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
+                ValidateIssuer = false,
+                ValidateAudience = false
             };
-            await userManager.CreateAsync(adminUser, "zaq1@WSX");
-
-            await userManager.AddToRoleAsync(adminUser, "Admin");
         }
-        catch (Exception ex)
-        {
-            var logger = services.GetRequiredService<ILogger<Program>>();
-            logger.LogError(ex, "An error occured during seeding process");
-        }
+    );
 
-        app.Run();
-    }
+
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+
+var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+                .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    var userManager = services.GetRequiredService<UserManager<AppUser>>();
+    var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
+
+    await context.Database.MigrateAsync();
+
+    await roleManager.CreateAsync(new AppRole { Name = "User" });
+    await roleManager.CreateAsync(new AppRole { Name = "Admin" });
+
+    var adminUser = new AppUser
+    {
+        UserName = "admin",
+        KnownAs = "Admin"
+    };
+    await userManager.CreateAsync(adminUser, "zaq1@WSX");
+
+    await userManager.AddToRoleAsync(adminUser, "Admin");
 }
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during seeding process");
+}
+
+app.Run();
